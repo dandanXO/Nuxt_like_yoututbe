f83be36bc0d734aee6ad50affac056e16b5207a5
Dandan <pigpig850130@gmail.com>
2018-09-02 15:23:48 +0800

    add sign in and sign up view

---
+++ pages/user/signin.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+                            <form>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                        name="email"
+                                        label="Mail"
+                                        id="email"
+                                        type="email"
+                                         v-model="email"
+                                        required>
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                         <v-text-field
+                                        name="password"
+                                        label="password"
+                                        id="password"
+                                        type="password"
+                                         v-model="password"
+                                        required>
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12> 
+                                        <v-btn type="submit">Sign In</v-btn>
+                                    </v-flex>
+                                </v-layout>
+                            </form>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:'',
+            password:'',
+            confirmPassword:''
+        }
+    },
+    computed:{
+      
+    },
+    methed:{
+        onSigninup(){
+            
+        }
+    }
+}
+</script>
